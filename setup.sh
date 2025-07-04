2ad5a9f2ccbc51f6f5362531888539f2ed4e8615
Fredrik Wildhagen <github@wildhagen.net>
2025-07-04 16:25:54 +0200

    Fix

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ set -e # Exit immediately if a command exits with a non-zero status.
 
 # Function to clean up mounts if the script exits prematurely
 cleanup_mounts() {
-    echo -e "\n\nAttempting to unmount /mnt..."
+    echo -e "\nAttempting to unmount /mnt..."
     umount -R /mnt &>/dev/null || true # Ignore errors if not mounted
     echo -e "CLEANUP COMPLETE."
 }
