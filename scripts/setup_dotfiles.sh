92b8dc8aa288dca0d90d7e45c46081e7b49b6659
Fredrik Wildhagen <github@wildhagen.net>
2025-07-05 17:46:20 +0200

    Fix

--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -29,23 +29,23 @@ function organize_home_directory() {
     echo "Organizing home directory..."
 
     echo "Renaming Documents to documents..."
-    mv -n "Documents" "documents" || { echo_color "RED" "Error: Could not rename Documents."; return 1; }
+    [ -d "Documents" ] && mv -n "Documents" "documents" || { echo_color "RED" "Error: Could not rename Documents."; return 1; }
     echo "Renaming Downloads to downloads..."
-    mv -n "Downloads" "downloads" || { echo_color "RED" "Error: Could not rename Downloads."; return 1; }
+    [ -d "Downloads" ] && mv -n "Downloads" "downloads" || { echo_color "RED" "Error: Could not rename Downloads."; return 1; }
 
     echo "Renaming and moving Desktop to documents/desktop..."
-    mv -n "Desktop" "documents/desktop" || { echo_color "RED" "Error: Could not rename and move Desktop."; return 1; }
+    [ -d "Desktop" ] && mv -n "Desktop" "documents/desktop" || { echo_color "RED" "Error: Could not rename and move Desktop."; return 1; }
     echo "Renaming and moving Music to documents/music..."
-    mv -n "Music" "documents/music" || { echo_color "RED" "Error: Could not rename and move Music."; return 1; }
+    [ -d "Music" ] && mv -n "Music" "documents/music" || { echo_color "RED" "Error: Could not rename and move Music."; return 1; }
     echo "Renaming and moving Pictures to documents/pictures..."
-    mv -n "Pictures" "documents/pictures" || { echo_color "RED" "Error: Could not rename and move Pictures."; return 1; }
+    [ -d "Pictures" ] && mv -n "Pictures" "documents/pictures" || { echo_color "RED" "Error: Could not rename and move Pictures."; return 1; }
     echo "Renaming and moving Videos to documents/videos..."
-    mv -n "Videos" "documents/videos" || { echo_color "RED" "Error: Could not rename and move Videos."; return 1; }
+    [ -d "Videos" ] && mv -n "Videos" "documents/videos" || { echo_color "RED" "Error: Could not rename and move Videos."; return 1; }
 
     echo "Deleting Public..."
-    rm -r "Public" || { echo_color "RED" "Error: Could not delete Public."; return 1; }
+    [ -d "Public" ] && rm -r "Public" || { echo_color "RED" "Error: Could not delete Public."; return 1; }
     echo "Deleting Templates..."
-    rm -r "Templates" || { echo_color "RED" "Error: Could not delete Templates."; return 1; }
+    [ -d "Templates" ] && rm -r "Templates" || { echo_color "RED" "Error: Could not delete Templates."; return 1; }
 
     return
 }
