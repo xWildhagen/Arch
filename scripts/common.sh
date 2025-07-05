25ab2f9032a0e3fa78fd3f71826e63745aee1147
Fredrik Wildhagen <github@wildhagen.net>
2025-07-05 15:50:21 +0200

    Fix

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 function enter_to_continue() {
     echo
-    read -p "PRESS ENTER TO CONTINUE..."
+    read -p "${BLUE}PRESS ENTER TO CONTINUE...${NC}"
 }
 
 function complete {
     echo
-    echo "$1 COMPLETE ---"
+    echo -e "${GREEN}$1 COMPLETE ---${NC}"
     enter_to_continue
 }
 
 function failed {
     echo
-    echo "$1 FAILED ---"
+    echo -e "${RED}$1 FAILED ---${NC}"
     enter_to_continue
 }
