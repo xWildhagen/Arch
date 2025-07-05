06df84403833cc8dfa18521ff1f62428d99e9931
Fredrik Wildhagen <github@wildhagen.net>
2025-07-05 02:25:57 +0200

    Fix

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 yay -S visual-studio-code-bin
+yay -S microsoft-edge-stable-bin
