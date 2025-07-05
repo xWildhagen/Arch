705cfab1222aa2c1b73887a1fae4ccfd14eb5235
Fredrik Wildhagen <github@wildhagen.net>
2025-07-05 16:50:16 +0200

    Fix

--- main.sh
+++ main.sh
@@ -8,18 +8,18 @@ function main() {
     while true; do
         clear
         echo -en "${BLUE}"
-        echo_dashes 40
+        echo_dashes 50
         echo "MAIN MENU"
-        echo_dashes 40
+        echo_dashes 50
         echo "1) Start Archinstall setup"
         echo "2) Start yay and package installation"
         echo "3) Start dotfiles setup"
         echo "0) Quit"
-        echo_dashes 40
+        echo_dashes 50
         echo -en "${PURPLE}"
         read -p "ENTER YOUR CHOICE: " OPTION
         echo -en "${BLUE}"
-        echo_dashes 40
+        echo_dashes 50
         echo -en "${NC}"
 
         case "$OPTION" in
@@ -35,7 +35,7 @@ function main() {
             0|q|Q)
                 echo_color "GREEN" "EXITING SCRIPT. GOODBYE!"
                 echo -en "${BLUE}"
-                echo "-------------------------------------"
+                echo_dashes 50
                 echo -en "${NC}"
                 break
                 ;;
@@ -43,7 +43,7 @@ function main() {
                 echo -en "${RED}"
                 read -p "INVALID OPTION. PRESS ENTER TO CONTINUE..."
                 echo -en "${BLUE}"
-                echo "-------------------------------------"
+                echo_dashes 50
                 echo -en "${NC}"
                 ;;
         esac
