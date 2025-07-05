4fbe173b46945d78532ce3ec57a815d9e70e8d53
Fredrik Wildhagen <github@wildhagen.net>
2025-07-05 15:54:07 +0200

    Fix

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+NC="\033[0m"
 
 function enter_to_continue() {
     echo
