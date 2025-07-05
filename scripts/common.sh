5ceaace8f7d8bf14742a375a6314f46acb36736c
Fredrik Wildhagen <github@wildhagen.net>
2025-07-05 19:49:51 +0200

    Fix

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-YELLOW="\033[0;33m"
-BLUE="\033[0;34m"
-PURPLE="\033[0;35m"
-NC="\033[0m"
+CONF_DIR="${HOME}/arch/scripts/conf"
+source "${CONF_DIR}/colors.conf"
 
 function enter_to_continue() {
     echo -en "${PURPLE}"
