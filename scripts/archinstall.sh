bdeb6b0a548a3af837e7c2899713c1878be265a8
Fredrik Wildhagen <github@wildhagen.net>
2025-07-05 12:18:58 +0200

    Fix

--- scripts/archinstall.sh
+++ scripts/archinstall.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+ARCHINSTALL_CONFIG="${HOME}/arch/archinstall/user_configuration.json"
+ARCHINSTALL_CREDS="${HOME}/arch/archinstall/user_credentials.json"
+
 function archinstall_main() {
     clear
-    echo "--- Running Archinstall setup ---"
+    echo "--- RUNNING ARCHINSTALL SETUP ---"
     echo
 
     archinstall_install
 
-    echo "--- Archinstall setup complete ---"
+    echo "--- ARCHINSTALL SETUP COMPLETE ---"
     echo
-    read -p "Press Enter to continue..."
+    read -p "PRESS ENTER TO CONTINUE..."
 }
 
 function archinstall_install() {
-    archinstall --config ~/arch/archinstall/user_configuration.json --creds ~/arch/archinstall/user_credentials.json
+    if [ ! -f "${ARCHINSTALL_CONFIG}" ]; then
+        echo "Error: Archinstall configuration file not found at ${ARCHINSTALL_CONFIG}"
+        exit 1
+    fi
+    if [ ! -f "${ARCHINSTALL_CREDS}" ]; then
+        echo "Error: Archinstall credentials file not found at ${ARCHINSTALL_CREDS}"
+        exit 1
+    fi
+    
+    archinstall --config "$ARCHINSTALL_CONFIG" --creds "$ARCHINSTALL_CREDS"
 }
