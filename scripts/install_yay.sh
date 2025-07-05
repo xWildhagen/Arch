060acecbc37e7ea2fb1971fc894e217f71de9000
Fredrik Wildhagen <github@wildhagen.net>
2025-07-05 17:19:32 +0200

    Fix

--- scripts/install_yay.sh
+++ scripts/install_yay.sh
@@ -25,29 +25,29 @@ function install_yay() {
     echo "Checking for existing yay installation..."
     if command -v yay &> /dev/null; then
         echo
-        echo "YAY IS ALREADY INSTALLED. SKIPPING INSTALLATION PROCESS."
+        echo_color "GREEN" "YAY IS ALREADY INSTALLED. SKIPPING INSTALLATION PROCESS."
         return 0
     fi
 
     echo "Yay not found. Proceeding with installation..."
     echo "Cloning and building yay..."
-    cd ${HOME} || { echo -e "\nError: Could not find home directory."; return 1; }
-    sudo pacman -S --needed git base-devel || { echo -e "\nError: Failed to install git and base-devel."; return 1; }
+    cd ${HOME} || { echo_color "RED" "\nError: Could not find home directory."; return 1; }
+    sudo pacman -S --needed git base-devel || { echo_color "RED" "\nError: Failed to install git and base-devel."; return 1; }
     if [ -d "yay" ]; then
         echo "yay directory already exists. Pulling latest changes..."
-        (cd yay && git pull) || { echo -e "\nError: Failed to pull latest yay changes."; return 1; }
+        (cd yay && git pull) || { echo_color "RED" "\nError: Failed to pull latest yay changes."; return 1; }
     else
-        git clone https://aur.archlinux.org/yay.git || { echo -e "\nError: Failed to clone yay repository."; return 1; }
+        git clone https://aur.archlinux.org/yay.git || { echo_color "RED" "\nError: Failed to clone yay repository."; return 1; }
     fi
 
-    cd yay || { echo -e "\nError: Could not find yay directory."; return 1; }
-    makepkg -si || { echo -e "\nError: Failed to build and install yay."; return 1; }
+    cd yay || { echo_color "RED" "\nError: Could not find yay directory."; return 1; }
+    makepkg -si || { echo_color "RED" "\nError: Failed to build and install yay."; return 1; }
 
-    cd ${HOME} || { echo -e "\nError: Could not find home directory."; return 1; }
+    cd ${HOME} || { echo_color "RED" "\nError: Could not find home directory."; return 1; }
     sudo rm -r yay
 
     echo
-    echo "YAY INSTALLED SUCCESSFULLY."
+    echo_color "GREEN" "YAY INSTALLED SUCCESSFULLY."
     return
 }
 
